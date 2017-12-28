91aa0e4efe36b0b8126435f4a5577714520c7e06
mihailrubinstejn <mrubinstein56@gmail.com>
2017-12-28 12:44:17 +0200

    Update properties

--- books/src/main/java/telran/books/controller/BooksHandler.java
+++ books/src/main/java/telran/books/controller/BooksHandler.java
@@ -1,6 +1,7 @@
 package telran.books.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,13 @@ import telran.books.interfaces.IBookConstant;
 import telran.books.interfaces.IBooks;
 
 @RestController
+@CrossOrigin
 public class BooksHandler {
 	@Autowired
 	IBooks books;
 	
 	@PostMapping(IBookConstant.BOOK)
+	@CrossOrigin
 	public boolean addBook(@RequestBody Book book) {
 		return books.addBook(book);
 	}
